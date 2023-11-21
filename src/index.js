e7038c48037abfd88305b150534b7933010dc7df
Edik <121885972+Solocomrade1101@users.noreply.github.com>
2023-11-21 09:52:52 +0300

    add files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './style.css'
+import logo from './assets/img/logo_welbex.svg'
+// import redBall from './assets/img/red_ball.svg'
+import redBall from './assets/img/red_ball.png'
+
+import purpleBall from './assets/img/purple_ball.svg'
+import telegram from './assets/img/telegram.svg'
+import viber from './assets/img/viber.svg'
+import watsapp from './assets/img/whatsapp.svg'
+
